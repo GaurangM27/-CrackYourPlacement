eab5439a637d33973783bd37159fbff4b60cfa50
Gaurang Mishra <118350406+GaurangM27@users.noreply.github.com>
2025-03-08 23:29:22 +0530

    Time: 0 ms (100%), Space: 10 MB (92.08%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,6 +5,8 @@ public:
             for(int j=i+1;j<matrix[0].size();j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
+        }
+        for(int i=0;i<matrix.size();i++){
             reverse(matrix[i].begin(),matrix[i].end());
         }
     }
